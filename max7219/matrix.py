3c78097190cebd7703cc1b552510b5a87dba7152
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-03-11 14:37:55 +0530

    Update class parameter with removing data types

--- max7219/matrix.py
+++ max7219/matrix.py
@@ -6,7 +6,7 @@ import max7219.font as font
 
 
 
-def toMhz(hz:int):
+def toMhz(hz):
     return hz * 1000 * 1000
 
 def toByte(array):
@@ -14,7 +14,7 @@ def toByte(array):
 
 class LedMatrix:
 
-    def __init__(self, open:int, port:int):
+    def __init__(self, open, port):
         self.spi = spidev.SpiDev()
 
         self.rows = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]
@@ -43,16 +43,16 @@ class LedMatrix:
         self.CLEAR_DISPLAY()
 
 
-    def SHUTDOWN_REGISTER(self, mode:int = 0x00):
+    def SHUTDOWN_REGISTER(self, mode = 0x00):
         self.spi.xfer([0x0f, mode])
 
-    def DISPLAY_TEST_REGISTER(self, mode:int = 0x00):
+    def DISPLAY_TEST_REGISTER(self, mode = 0x00):
         self.spi.xfer([0x0f, mode])
 
-    def SCAN_LIMIT_REGISTER(self, range:int = 0x07):
+    def SCAN_LIMIT_REGISTER(self, range = 0x07):
         self.spi.xfer([0x0b, range])
 
-    def DECODE_MODE_REGISTER(self, mode:int = 0x00):
+    def DECODE_MODE_REGISTER(self, mode = 0x00):
         self.spi.xfer([0x09, mode])
 
     def CLEAR_DISPLAY(self):
@@ -71,7 +71,7 @@ class LedMatrix:
         for i in range(8):
             self.spi.xfer([self.rows[i], bitMap[i]])
 
-    def show(self, string:str, delay:int = 0):
+    def show(self, string, delay = 0):
         for char in  string:
             if char == " ":
                 self.CLEAR_DISPLAY()
